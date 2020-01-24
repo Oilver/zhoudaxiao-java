63f204457e256bf19ca74ba735a01fbd64b1f20a
harvey <543527507@qq.com>
2020-01-24 18:18:13 +0800

    增加了限流功能：   同一IP一秒内最多发起10次请求，其他都拒绝

--- src/main/java/com/yiseven/zhoudaxiao/common/interceptor/LimitRequestInterceptor.java
+++ src/main/java/com/yiseven/zhoudaxiao/common/interceptor/LimitRequestInterceptor.java
@@ -59,19 +59,7 @@ public class LimitRequestInterceptor implements HandlerInterceptor {
      * @return
      */
     private String getIpAddress(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
+        String ip = request.getHeader("X-Forwarded-For");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
